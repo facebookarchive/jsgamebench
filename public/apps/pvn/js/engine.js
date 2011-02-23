88ad2e36102e6a70acf82a539cc60f8954d45181
coryondrejka <cory.ondrejka@fb.com>
2011-02-22 18:00:14 -0800

    removed recreational hash lookup

--- public/apps/pvn/js/engine.js
+++ public/apps/pvn/js/engine.js
@@ -40,7 +40,6 @@ var win_size = [];
     visualToGob: function(visual) {
       var gob = visual.gob;
       if (gob) {
-        var el = Sprites.spritedictionary[visual.name].imageel;
         gob.pos[0] = visual.x * this.scale - cam_pos[0];
         gob.pos[1] = visual.y * this.scale - cam_pos[1];
         gob.theta = visual.angle || 0;
