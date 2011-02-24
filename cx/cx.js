0f5365110e173a51ee6490bee57c68e1f50a2ddc
coryondrejka <cory.ondrejka@fb.com>
2011-02-24 00:08:08 -0800

    pirachessrarararar

--- cx/cx.js
+++ cx/cx.js
@@ -8,44 +8,23 @@ function loadImageList(path,list) {
   }
 }
 
-var pirate,p2;
-var pirate_dir = 1;
-var rot = 0;
-
 function tick() {
-  pirate.pos[0] += pirate_dir;
-  if (pirate.pos[0] < 0) {
-    pirate_dir = 1;
-  } else if (pirate.pos[0] > JSGlobal.w) {
-    pirate_dir = -1;
-  }
-  pirate.dirty = 1;
-  rot += 0.02;
-  if (rot > 2*Math.PI) {
-    rot -= 2*Math.PI;
-  }
-  p2.pos[0] = 200 + Math.sin(rot) * 100;
-  p2.pos[1] = 200 + Math.cos(rot) * 100;
-  p2.dirty = true;
 
   if (JSGlobal.mouse.buttons[0]) {
     JSGlobal.mouse.buttons[0] = 0;
     var dx = JSGlobal.mouse.x;
     var dy = JSGlobal.mouse.y;
-    console.log([dx.dy]);
+    Pieces.select(dx,dy);
   }
 }
 
-var size = 75;
-
 function postImageLoad() {
-  World.add('bg_idx', 'background', [JSGlobal.w*0.5,JSGlobal.h*0.5], 0);
-  pirate = Gob.add(Utils.uuidv4(), 'bouncing_pirate', 0, [100,150], [0,0], 10, 1);
-  p2 = Gob.add(Utils.uuidv4(), 'bouncing_pirate', 0, [100,150], [0,0], 10, 1);
+  Board.init();
+  Pieces.init();
 }
 
 function clickButton() {
-  pirate_dir = -pirate_dir;
+
 }
 
 function init() {
@@ -61,16 +40,18 @@ function init() {
 
   GameFrame.setXbyY();
   UI.hookUIEvents('gamebody');
-  loadImageList('/pvn/images/',['bouncing_pirate.png','background.jpg']);
-  ClientCmd.install('clickButton',clickButton);
+  loadImageList('/pvn/images/',['bouncing_pirate.png']);
 }
 
 function resize() {
-  UI.addCollection('', 'buttons', {pos: [0, 0]});
-  UI.addButton('buttons', 'optin', {pos: [0, 0], width: 150, height: 60, fontsize: '300%', text: 'ClickMe!', command: {cmd: 'clickButton' }});
-  //console.log('resize');
 }
 
-Init.setFunctions({app: tick, init: init, draw: Render.tick, ui: UI.tick, resize: resize, postLoad: postImageLoad, fps:1000 });
+function draw() {
+  Pieces.tick();
+  Board.tick();
+  Render.tick();
+}
+
+Init.setFunctions({app: tick, init: init, draw: draw, ui: UI.tick, resize: resize, postLoad: postImageLoad, fps:1000 });
 
 })();
